62d67e5cd49bc94f253af601b2804915f808b6aa
wade <wadechen@outlook.com>
2026-04-23 17:01:51 +0800

    test

--- custom/segment_train_gpu.py
+++ custom/segment_train_gpu.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolo26m.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo26n-seg.pt")  # load a pretrained model (recommended for training)
     # model.to("cuda")
 
     # Train the model
     results = model.train(
-        data="datasets/african-wildlife.yaml",
+        data="custom/package-seg.yaml",
         epochs=100,
         imgsz=640,
         device=0,
